afc8b0d2a940346681d7ea4956e1a211053bb47c
Tyrone Wong <tyrone.m.wong@gmail.com>
2020-06-04 02:03:28 -0400

    images show from dropbox api

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -9,17 +9,12 @@ const requestPromise = require("request-promise");
 
 let sessionCache = new nodeCache();
 
-function renderImage(req, res, next){
+async function renderImage(req, res, next){
 
     let token = sessionCache.get("aTempTokenKey");
 
     if (token){
-        let photoPaths = [];
-        fs.readdir(photoPath, (err, photos)=>{
-            photos.forEach( photoName => {
-                photoPaths.push(photoName);
-            })
-        })
+        let photoPaths = await fetchPhotoLinks(token);
         res.render('index', {images:photoPaths,  layout:false})
     }
     else{
@@ -85,6 +80,73 @@ async function oauthRedirect(req, res, next) {
         return next(new Error('Token Error'));
     }
 }
+
+
+async function fetchPhotoLinks(token){
+    let imagePaths = await fetchImagePaths(token);
+
+    let tempLinksResponse = await fetchTempLinksFromPaths(token, imagePaths.paths);
+    let tempLinkList = tempLinksResponse.map(function(entry){
+        return entry.link;
+    })
+    return tempLinkList;
+}
+
+
+
+async function fetchImagePaths(token){
+
+    let options={
+      url: config.DROPBOX_API_DOMAIN + config.DROPBOX_LIST_FOLDER_PATH, 
+      headers:{"Authorization":"Bearer " + token},
+      method: 'POST',
+      json: true ,
+      body: {"path":""}
+    }
+  
+    try{
+      //Make request to Dropbox to get list of file entry object
+      let fileList = await requestPromise(options);
+  
+
+      // Retrive the lowercase paths
+      let lowercasePaths = result.map(function (entry) {
+        return entry.path_lower;
+      });
+  
+      //return a cursor only if there are more files in the current folder
+      let response= {};
+      response.paths= lowercasePaths;       
+      return response;
+  
+    }catch(error){
+      return next(new Error('error listing folder. '+error.message));
+    }        
+} 
+  
+  
+  //Returns an array with temporary links from an array with file paths
+  function fetchTempLinksFromPaths(token,paths){
+  
+    var promises = [];
+    let options={
+      url: config.DROPBOX_API_DOMAIN + config.DROPBOX_GET_TEMPORARY_LINK_PATH, 
+      headers:{"Authorization":"Bearer "+token},
+      method: 'POST',
+      json: true
+    }
+  
+    //Create a promise for each path and push it to an array of promises
+    paths.forEach((path_lower)=>{
+      options.body = {"path":path_lower};
+      promises.push(requestPromise(options));
+    });
+  
+    //await till all promises are completed within the array of temp links
+    return Promise.all(promises);
+  }
+
+
 
 module.exports = {
     renderImage:renderImage,
